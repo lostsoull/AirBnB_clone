c57e8d27f34b161d4a3d616bf06ef065d33d5e90
HANANEZOUBI <hananezoubi1@gmail.com>
2024-02-11 15:27:25 -0800

    h

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun  5 15:43:09 2020
-@author: meco
+Created on February 2024
+@author: yassine abdourrabih and hanane zoubi
 """
 import sys
 import unittest
@@ -14,43 +14,31 @@ from console import HBNBCommand
 
 
 class TestHBNBCommand(unittest.TestCase):
-    """
-    class for testing HBNBCommand class' methods
-    """
+    """to test HBNBCommand methods of the class"""
 
     @classmethod
     def setUpClass(cls):
-        """
-        Set up class method for the doc tests
-        """
+        """Set up class method"""
         cls.setup = inspect.getmembers(HBNBCommand, inspect.isfunction)
 
     def test_pep8_conformance_HBNBCommand(self):
-        """
-        Test that console.py file conform to PEP8
-        """
+        """to test that console.py file conform to PEP8"""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/console.py'])
         self.assertEqual(result.total_errors, 1,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_HBNBCommand(self):
-        """
-        Test that test_console.py file conform to PEP8
-        """
+        """To test that test_console.py file conform to PEP8"""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_models/test_console.py'])
         self.assertEqual(result.total_errors, 1,
                          "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
-        """
-        Tests if module docstring documentation exist
-        """
+        """To test of module docstring documentation (exist)"""
         self.assertTrue(len(HBNBCommand.__doc__) >= 1)
 
     def test_class_docstring(self):
-        """
-        Tests if class docstring documentation exist
-        """
+        """To test of class docstring documentation (exist)"""
         self.assertTrue(len(HBNBCommand.__doc__) >= 1)
